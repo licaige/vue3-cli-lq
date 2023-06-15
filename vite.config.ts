cad521e971ae8d78688f93f1d832c7c529996c87
“李琦” <qi.li@hongpucorp.com>
2023-06-15 15:51:10 +0800

    特性(vite配置): 打包处理

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,6 @@
 import { defineConfig, ConfigEnv, UserConfig, loadEnv } from 'vite'
 import path from 'path'
+import { resolve as readsolve } from 'path'
 // vite.config.ts中无法使用import.meta.env 所以需要引入
 import vue from '@vitejs/plugin-vue'
 import { createSvgIconsPlugin } from 'vite-plugin-svg-icons'
@@ -11,11 +12,15 @@ import viteCompression from 'vite-plugin-compression'
 // import AutoImport from 'unplugin-auto-import/vite'
 // import Components from 'unplugin-vue-components/vite'
 //import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
-
+// 当前时间戳
+const Timestamp = new Date().getTime()
 function resolve(dir) {
   return path.join(__dirname, '.', dir)
 }
-
+/** 路径查找 */
+const pathResolve = (dir: string): string => {
+  return readsolve(__dirname, '.', dir)
+}
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }: ConfigEnv): UserConfig => {
   return {
@@ -92,5 +97,24 @@ export default defineConfig(({ mode }: ConfigEnv): UserConfig => {
     //     },
     //   },
     // },
+    build: {
+      sourcemap: false,
+      // 消除打包大小超过500kb警告
+      chunkSizeWarningLimit: 4000,
+      rollupOptions: {
+        input: {
+          index: pathResolve('index.html'),
+        },
+        // 静态资源分类打包
+        output: {
+          // chunkFileNames: 'static/js/[name]-[hash].js',
+          // entryFileNames: 'static/js/[name]-[hash].js',
+          // assetFileNames: 'static/[ext]/[name]-[hash].[ext]',
+          chunkFileNames: `static/js/[name]-[hash]-${Timestamp}.js`,
+          entryFileNames: `static/js/[name]-[hash]-${Timestamp}.js`,
+          assetFileNames: `static/[ext]/[name]-[hash]-${Timestamp}.[ext]`,
+        },
+      },
+    },
   }
 })
