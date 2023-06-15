7e211c2dafbec79fceec146f375b6309b2e2c403
“李琦” <qi.li@hongpucorp.com>
2023-06-15 15:29:57 +0800

    格式: 格式化

--- .eslintrc.js
+++ .eslintrc.js
@@ -21,5 +21,8 @@ module.exports = {
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
     'no-unused-vars': 'off',
+    '@typescript-eslint/no-unused-vars': 'off',
+    'no-async-promise-executor': 'off',
+    'vue/multi-word-component-names': 'off',
   },
 }
